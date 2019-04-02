d7a2edfde268abfe4da97933c8c1dfe7091bc624
Kimberly Caras <kcaras6@gatech.edu>
2019-04-02 16:53:36 -0400

    added a cnn to find the score of the reddit joke data

---
+++ humor/rl/data_parsing.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+from torchtext.data import Field
+from torchtext.data import TabularDataset
+import torch.autograd as ag
+from torch.autograd import Variable
+import torch.cuda as cuda
+import json
+import unicodedata
+import re
+
+USE_CUDA = False
+
+tokenize = lambda x: x.split()
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+
+    def index_words(self, sentence):
+        for word in sentence.split(' '):
+            self.index_word(word)
+
+    def index_word(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+
+# Turn a Unicode string to plain ASCII, thanks to http://stackoverflow.com/a/518232/2809427
+def unicode_to_ascii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+
+# Lowercase, trim, and remove non-letter characters
+def normalize_string(s):
+    #s = unicode_to_ascii(s.lower().strip())
+    s = s.lower().strip()
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+
+def read_langs(lang_name, json_out, write_clean_file=False, write_file_type='csv'):
+    pairs = []
+    print("Reading lines...")
+    # Read the file and split into lines
+    lines = ['']*len(json_out.keys())
+
+    clean_map_train = {}
+    clean_map_test = {}
+    clean_map_valid = {}
+    train_ix = int(len(json_out.keys())*0.7)
+    valid_ix = int(len(json_out.keys())*0.2)
+    for i, s in enumerate(json_out.keys()):
+        clean_string = normalize_string(s)
+        lines[i] = clean_string
+        if i < train_ix:
+            clean_map_train[clean_string] = float(json_out[s])
+        elif i > train_ix and i < train_ix + valid_ix:
+            clean_map_valid[clean_string] = float(json_out[s])
+        else:
+            clean_map_test[clean_string] = float(json_out[s])
+        pairs.append((clean_string, json_out[s]))
+    if write_clean_file:
+        maps = {'train': clean_map_train, 'valid': clean_map_valid, 'test': clean_map_test}
+        for f_name in ['train', 'valid', 'test']:
+            if write_file_type == 'json':
+                with open('reddit_cleaned_{}.json'.format(f_name), 'w') as outfile:
+                    json.dump(maps[f_name], outfile)
+            elif write_file_type == 'csv':
+                lines = [','.join([k , str(maps[f_name][k])]) for k in maps[f_name].keys()]
+                doc = '\n'.join(lines)
+                with open('reddit_cleaned_{}.csv'.format(f_name), 'w') as outfile:
+                    outfile.write(doc)
+
+    #lines = [normalize_string(s) for s in json_out.keys()]
+    # Reverse pairs, make Lang instances
+    output_lang = Lang(lang_name)
+    print("Indexing words...")
+    for line in lines:
+        output_lang.index_words(line)
+    return output_lang, pairs
+
+
+def parse_reddit_jokes():
+    file_name = 'reddit_jokes.json'
+    #lines = []
+    #scores = []
+    map = {}
+    with open(file_name, 'r') as json_file:
+        data = json.load(json_file)
+        for d in data:
+            map[d['body']] = float(d['score'])
+            #lines.append(d['body'])
+            #scores.append(float(d['score']))
+    return map
+
+
+def indexes_from_sentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+SOS_token = 0
+EOS_token = 1
+
+
+def variable_from_sentence(lang, sentence):
+    indexes = indexes_from_sentence(lang, sentence)
+    indexes.append(EOS_token)
+    var = Variable(torch.LongTensor(indexes).view(-1, 1))
+#     print('var =', var)
+    if USE_CUDA: var = var.cuda()
+    return var
+
+
+def make_torch_dataset_from_reddit_jokes():
+    TEXT = Field(sequential=True, tokenize=tokenize, lower=True)
+    LABEL = Field(sequential=False, use_vocab=False, dtype=torch.float)
+    joke_datafields = [('sentence', TEXT), ('score', LABEL)]
+    trn, vld, tst = TabularDataset.splits(path='', train='reddit_cleaned_train.csv',
+                                          validation='reddit_cleaned_valid.csv',
+                                          test='reddit_cleaned_test.csv',
+                                          format='csv',
+                                          fields=joke_datafields)
+    return trn, vld, tst, TEXT, LABEL
+
+
+if __name__ == '__main__':
+    map = parse_reddit_jokes()
+    read_langs('jokes', map, write_clean_file=True, write_file_type='csv')
--- humor/rl/joke_scoring.py
+++ humor/rl/joke_scoring.py
@@ -1,11 +1,16 @@
-import sklearn
-import json
+from CNN_Humor import CNN
+import data_parsing
 import pickle
-import torch
+from torch import optim
+from torch import nn
+from torchtext import data
+import spacy
 import numpy as np
+import torch
+import time
 UNK_TOKEN = '<UNK>'
 
-
+# https://github.com/bentrevett/pytorch-sentiment-analysis/blob/master/4%20-%20Convolutional%20Sentiment%20Analysis.ipynb
 
 def obtain_polyglot_embeddings(filename, word_to_ix):
     vecs = pickle.load(open(filename, 'rb'), encoding='latin1')
@@ -28,45 +33,158 @@ def obtain_polyglot_embeddings(filename, word_to_ix):
     word_embeddings = np.array(word_embeddings)
     return word_embeddings
 
-def initialize_with_pretrained(pretrained_embeds, word_embedding, use_cuda=False):
-    '''
-    Initialize the embedding lookup table of word_embedding with the embeddings
-    from pretrained_embeds.
-    Remember that word_embedding has a word_to_ix member you will have to use.
-    For every word that we do not have a pretrained embedding for, keep the default initialization.
-    NOTE: don't forget the UNK token!
-    :param pretrained_embeds: dict mapping word to python list of floats (the embedding
-        of that word)
-    :param word_embedding: The network component to initialize (i.e, a BiLSTMWordEmbedding)
-    '''
-    for word, index in word_embedding.word_to_ix.items():
+
+def initialize_with_pretrained(pretrained_embeds, word_embedding, lang, use_cuda=False):
+    for word, index in lang.word2index.items():
         if word in pretrained_embeds:
-            word_embedding.word_embeddings.weight.data[index] = torch.Tensor(pretrained_embeds[word])
+            #print("found word {}".format(word))
+            word_embedding.embedding.weight.data[index] = torch.Tensor(pretrained_embeds[word])
         else:
-            word_embedding.word_embeddings.weight.data[index] = torch.Tensor(pretrained_embeds[UNK_TOKEN])
+            word_embedding.embedding.weight.data[index] = torch.Tensor(pretrained_embeds[UNK_TOKEN])
 
 
-def parse_reddit_jokes():
-    file_name = 'reddit_jokes.json'
-    lines = []
-    scores = []
-    with open(file_name, 'r') as json_file:
-        data = json.load(json_file)
-        for d in data:
-            lines.append(d['body'])
-            scores.append(float(d['score']))
-    return lines, scores
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+
+    #round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds == y).float() #convert into float for division
+    acc = correct.sum() / len(correct)
+    return acc
 
 
-def train_embedder():
-    embedding_dim = 30
-    hidden_dim = 30
-    model = bilstm.BiLSTM(len(word_to_ix), tag_to_ix, embedding_dim, hidden_dim)
+def train(model, iterator, optimizer, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.train()
+
+    for batch in iterator:
+        optimizer.zero_grad()
+
+        predictions = model(batch.sentence).squeeze(1)
+
+        loss = criterion(predictions, batch.score)
+
+        acc = binary_accuracy(predictions, batch.score)
+
+        loss.backward()
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
 
-def train_scorer():
-    pass
+def evaluate(model, iterator, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
 
+    model.eval()
+
+    with torch.no_grad():
+        for batch in iterator:
+            predictions = model(batch.sentence).squeeze(1)
+
+            loss = criterion(predictions, batch.score)
+
+            acc = binary_accuracy(predictions, batch.score)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
 
 def run():
-    pass
+    json_out = data_parsing.parse_reddit_jokes()
+    out_lang, pairs = data_parsing.read_langs('reddit_jokes', json_out)
+    INPUT_DIM = out_lang.n_words
+    EMBEDDING_DIM = 64
+    N_FILTERS = 100
+    FILTER_SIZES = [3, 4, 5]
+    OUTPUT_DIM = 1
+    DROPOUT = 0.5
+    model = CNN(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT)
+    pret_embs = pickle.load(open('pretrained-embeds-coref.pkl', 'rb'))
+    initialize_with_pretrained(pret_embs, model, out_lang)
+
+    N_EPOCHS = 5
+    BATCH_SIZE = 64
+    best_valid_loss = float('inf')
+    optimizer = optim.Adam(model.parameters())
+    criterion = nn.BCEWithLogitsLoss()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    criterion = criterion.to(device)
+    # train_ix = int(len(pairs)*0.7)
+    # valid_ix = int(len(pairs)*0.2)
+    # train_data = pairs[:train_ix]
+    # valid_data = pairs[train_ix: train_ix + valid_ix]
+    # test_data = pairs[train_ix + valid_ix:]
+    MAX_VOCAB_SIZE = 25_000
+    train_data, valid_data, test_data, TEXT, LABEL = data_parsing.make_torch_dataset_from_reddit_jokes()
+    TEXT.build_vocab(train_data,
+                     max_size=MAX_VOCAB_SIZE,
+                     vectors="glove.6B.100d",
+                     unk_init=torch.Tensor.normal_)
+    #LABEL.build_vocab(train_data)
+    train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+        (train_data, valid_data, test_data),
+        batch_size=BATCH_SIZE,
+        device=device)
+    print('starting the training thing')
+    for epoch in range(N_EPOCHS):
+
+        start_time = time.time()
+
+        train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+        valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+
+        end_time = time.time()
+
+        epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+        if valid_loss < best_valid_loss:
+            best_valid_loss = valid_loss
+            torch.save(model.state_dict(), 'tut4-model.pt')
+
+        print(f'Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+        print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
+        print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc * 100:.2f}%')
+
+    model.load_state_dict(torch.load('tut4-model.pt'))
+
+    test_loss, test_acc = evaluate(model, test_iterator, criterion)
+
+    print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc * 100:.2f}%')
+
+
+
+# nlp = spacy.load('en')
+#
+# def predict_humor(sentence, min_len = 5):
+#     tokenized = [tok.text for tok in nlp.tokenizer(sentence)]
+#     if len(tokenized) < min_len:
+#         tokenized += ['<pad>'] * (min_len - len(tokenized))
+#     indexed = [TEXT.vocab.stoi[t] for t in tokenized]
+#     tensor = torch.LongTensor(indexed).to(device)
+#     tensor = tensor.unsqueeze(1)
+#     prediction = torch.sigmoid(model(tensor))
+#     return prediction.item()
+
+
+
+
+if __name__ == '__main__':
+    run()
